282734bb824e97d07c0b32a7108c20ff611cd308
parth <parth.natu01@gmail.com>
2021-04-11 00:25:37 +0530

    added search functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 // let personData = require('./personData').personData;
  //import { personData } from './personData.js'
  //var personData ;
-function populateChatData(){
-       var readChatData = new ReadChatData();
 
+ var funcCallCount = 0;
+function populateChatData(){
+  const processChange = debounce(() => saveInput());
+  document.getElementById('search').addEventListener('input', processChange)
+  document.getElementById('chatcontainer').innerHTML="";
+  document.getElementById('search').hidden = true;
+       var readChatData = new ReadChatData();
+       document.getElementById('container').style.display = 'grid';
       readChatData.readData().then((personData) => {
         document.getElementById('individualChat').style.display = 'none';
         var chatElement = document.getElementById('chatcontainer');
@@ -24,25 +30,28 @@ function populateChatData(){
               ?
               element.messages.their[element.messages.their.length - 1]
              : element.messages.yours[element.messages.yours.length - 1] ;
-             var lastMsg = lastMsgObj.message;
-             lastMsgDiv.innerHTML = "<div class='boldText'> "+element.name+"</div>";
-             lastMsgDiv.innerHTML +="<div class='fadedText'>"+ lastMsg + "</div>";
-             lastMsgDiv.classList.add('border-bottom');
+             var lastMsg = lastMsgObj.message.length > 20 ? lastMsgObj.message.slice(0,20)+'...' :lastMsgObj.message ;
+             lastMsgDiv.innerHTML = "<div class='boldText largeText'> "+element.name+"</div>";
+             lastMsgDiv.innerHTML +="<div class='fadedText mediumText'>"+ lastMsg + "</div>";
+            // lastMsgDiv.classList.add('border-bottom');
      
              var lastMsgTime = new Date(lastMsgObj.timestamp).getHours() + ":" + new Date(lastMsgObj.timestamp).getMinutes();
              var unreadMsg = document.createElement('div')
              unreadMsg.innerHTML = "<p>"+lastMsgTime+"</p>";
-             unreadMsg.classList.add('boldText')
+             unreadMsg.classList.add('timeAndUnread','boldText','mediumText')
      
              if(element.messages.unread == 0)
               unreadMsg.classList.add('fadedText')
              else            
               unreadMsg.innerHTML += "<p class='unread'>"+ element.messages.unread.toString() + "</p>" ;
              
-             
+             var borderElem = document.createElement('div')
+             borderElem.className = 'border-bottom';
+             borderElem.style="grid-column:2/4; margin-right: 2em"
              elem.appendChild(imageDiv)
              elem.appendChild(lastMsgDiv);
              elem.appendChild(unreadMsg);
+             elem.appendChild(borderElem)
              elem.addEventListener('click', ()=>{openChatForUser(element.id)})
              //  chatElement.onclick = openChatForUser(element.id);
                chatElement.appendChild(elem);
@@ -60,9 +69,13 @@ function openChatForUser(id){
 }
 
 function populateContentForIndividualChat(id){
+    document.getElementById('individualChat').innerHTML="";
     var readChatData = new ReadChatData();
      var nameHeader = document.createElement('div')
      var arrow = document.createElement('span');
+     arrow.addEventListener('click',()=>{
+       populateChatData();
+     })
      arrow.style.float = 'left';
      arrow.innerHTML = '<i class="fa fa-arrow-left" style="margin-top: 1em"></i>';
      nameHeader.appendChild(arrow);
@@ -84,88 +97,25 @@ function populateContentForIndividualChat(id){
 
 }
 
+function searchBarVisiblity(status){
+  if(status == "visible")
+  document.getElementById('search').hidden = false;
+  else 
+  { 
+    document.getElementById('search').hidden = true;
+    populateChatData();
+  }
+  }
 
-// let personData = [
-//     {
-//      name: 'Parth Natu',
-//      id: 1,
-//      imageUrl: 'assets/AVATAR/image_part_001.jpg',
-//      messages: {
-//          their: [
-//              {message: 'hello', timestamp: new Date(2018, 11, 24, 10, 33, 30, 0)},
-//              {message: "I'm good! How are you?",  timestamp: new Date(2018, 11, 24, 10, 33, 34, 0) },
-//              {message: "I'm good! How are you?",  timestamp: new Date(2018, 11, 24, 10, 33, 38, 0) },
-//              {message: "I'm good! How are you?",  timestamp: new Date(2018, 11, 24, 10, 33, 40, 0) },
-//              {message: "I'm good! How are you?",  timestamp: new Date(2018, 11, 24, 10, 33, 42, 0) }
-//          ],
-//          yours: [
-//           {message: 'hello! how are you', timestamp: new Date(2018, 11, 24, 10, 31, 30, 0)}, 
-//           {message: "I'm doing great! Thanks for asking", timestamp: new Date(2018, 11, 24, 10, 35, 30, 0)},
-        
-//          ],
-//          unread: 12
-//       }
-//     },
-//     {
-//       name: 'Samarth Joshi',
-//       id: 2,
-//       imageUrl: 'assets/AVATAR/image_part_002.jpg',
-//       messages: {
-//           their: [
-//               {message: 'hello', timestamp: new Date(2018, 11, 24, 10, 33, 30, 0)},
-//               {message: "I'm good! How are you?",  timestamp: new Date(2018, 11, 24, 10, 33, 34, 0) },
-//               {message: "I'm good! How are you?",  timestamp: new Date(2018, 11, 24, 10, 33, 38, 0) },
-//               {message: "I'm good! How are you?",  timestamp: new Date(2018, 11, 24, 10, 33, 40, 0) },
-//               {message: "I'm good! How are you?",  timestamp: new Date(2018, 11, 24, 10, 33, 42, 0) }
-//           ],
-//           yours: [
-//            {message: 'hello! how are you', timestamp: new Date(2018, 11, 24, 10, 31, 30, 0)}, 
-//            {message: "I'm doing great! Thanks for asking", timestamp: new Date(2018, 11, 24, 10, 35, 30, 0)},
-         
-//           ],
-//           unread: 3
-//        }
-//      },
-//      {
-//       name: 'Lokesh Karkare',
-//       id: 3,
-//       imageUrl: 'assets/AVATAR/image_part_003.jpg',
-//       messages: {
-//           their: [
-//               {message: 'hello', timestamp: new Date(2018, 11, 24, 10, 33, 30, 0)},
-//               {message: "I'm good! How are you?",  timestamp: new Date(2018, 11, 24, 10, 33, 34, 0) },
-//               {message: "I'm good! How are you?",  timestamp: new Date(2018, 11, 24, 10, 33, 38, 0) },
-//               {message: "I'm good! How are you?",  timestamp: new Date(2018, 11, 24, 10, 33, 40, 0) },
-//               {message: "I'm good! How are you?",  timestamp: new Date(2018, 11, 24, 10, 33, 42, 0) }
-//           ],
-//           yours: [
-//            {message: 'hello! how are you', timestamp: new Date(2018, 11, 24, 10, 31, 30, 0)}, 
-//            {message: "I'm doing great! Thanks for asking", timestamp: new Date(2018, 11, 24, 10, 35, 30, 0)},
-         
-//           ],
-//           unread: 0
-//        }
-//      },
-//      {
-//       name: 'Satyam Singh',
-//       id: 4,
-//       imageUrl: 'assets/AVATAR/image_part_004.jpg',
-//       messages: {
-//           their: [
-//               {message: 'hello', timestamp: new Date(2018, 11, 24, 10, 33, 30, 0)},
-//               {message: "I'm good! How are you?",  timestamp: new Date(2018, 11, 24, 10, 33, 34, 0) },
-//               {message: "I'm good! How are you?",  timestamp: new Date(2018, 11, 24, 10, 33, 38, 0) },
-//               {message: "I'm good! How are you?",  timestamp: new Date(2018, 11, 24, 10, 33, 40, 0) },
-//               {message: "I'm good! How are you?",  timestamp: new Date(2018, 11, 24, 10, 33, 42, 0) }
-//           ],
-//           yours: [
-//            {message: 'hello! how are you', timestamp: new Date(2018, 11, 24, 10, 31, 30, 0)}, 
-//            {message: "I'm doing great! Thanks for asking", timestamp: new Date(2018, 11, 24, 10, 36, 30, 0)},
-         
-//           ],
-//           unread: 0
-//        }
-//      },
-  
-  
-//   ];
+  function debounce(func, timeout = 1500){
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+  };
+}
+function saveInput(){
+  var readChatData = new ReadChatData();
+  readChatData.searchMessages(document.getElementById('searchInput').value);
+}
+
--- service/readChatData.js
+++ service/readChatData.js
@@ -35,13 +35,77 @@ class ReadChatData{
          chatDiv.innerHTML = chatHtml;
          document.getElementById('individualChat').appendChild(chatDiv);
          var inputDiv = document.createElement('div');
+         inputDiv.classList.add('textAreaInput');
        //  inputDiv.innerHTML = '<input type="text" placeholder="Type your message" >'
-       inputDiv.innerHTML = '<textarea type="text"> Type your message </textarea> '
+       var msgElement = document.createElement('textarea')
+       msgElement.setAttribute('id', 'messageArea');
+       msgElement.setAttribute('placeholder', 'Type new message');
+       
+       
+       msgElement.addEventListener('input',()=>{
+        
+          msgElement.style.height = ""; 
+          msgElement.style.height = Math.min(msgElement.scrollHeight, 200) + "px";
+           })
+           inputDiv.innerHTML += '<div class="sendIcon"> <i style="color: white;" class="fa fa-paper-plane"></i></div>'
+           inputDiv.appendChild(msgElement)
          document.getElementById('individualChat').appendChild(inputDiv)
 
      })
  }
 
+ searchMessages(input){
+   this.readData().then(data => {
+    var messages=[];
+     data.forEach(person => {
+       messages.push(...person.messages.their.concat(person.messages.yours).map((msg)=> {return  {'imageUrl': person.imageUrl,'name' : person.name, ...msg}}))
+     })
+    
+    this.loadSearchData(messages.filter(elem => elem.message.includes(input)), input);
+   })
+    
+ }
+ loadSearchData(data, query){
+  document.getElementById('chatcontainer').innerHTML = "";
+  data.forEach(element => {
+    var elem = document.createElement('div');
+    elem.classList.add('chat')
+    
+    var imageDiv = document.createElement('div')
+    imageDiv.style.justifySelf = 'center';
+    var imageTag = document.createElement('img')
+    imageTag.src = element.imageUrl
+    imageDiv.appendChild(imageTag)
+    var lastMsgDiv = document.createElement('div')
+    //var lastMsg = element.message.length > 20 ? element.message.slice(0,20)+'...' :element.message ;
+    var lastMsg = element.message;
+    var index = lastMsg.indexOf(query);
+    lastMsgDiv.innerHTML = "<div class='boldText largeText'> "+element.name+"</div>";
+    lastMsgDiv.innerHTML +="<div class='fadedText mediumText'>"+ lastMsg.substring(index-20>0?index-20:0,index) + 
+    "<span class='highlight'>" + lastMsg.substring(index,index+query.length) + "</span>" 
+    + lastMsg.substring(index+query.length,lastMsg.length>20? index+query.length: lastMsg.length) + "</div>";
+    //lastMsgDiv.classList.add('border-bottom');
 
+    var lastMsgTime = new Date(element.timestamp).getHours() + ":" + new Date(element.timestamp).getMinutes();
+    var unreadMsg = document.createElement('div')
+    unreadMsg.innerHTML = "<p>"+lastMsgTime+"</p>";
+    //unreadMsg.classList.add('boldText')
+
+    
+     unreadMsg.classList.add('timeAndUnread','fadedText','mediumText')
+     var borderElem = document.createElement('div')
+     borderElem.className = 'border-bottom';
+     borderElem.style="grid-column:2/4; margin-right: 2em";
+    
+   
+    elem.appendChild(imageDiv)
+    elem.appendChild(lastMsgDiv);
+    elem.appendChild(unreadMsg);
+    elem.appendChild(borderElem)
+    //elem.addEventListener('click', ()=>{openChatForUser(element.id)})
+    //  chatElement.onclick = openChatForUser(element.id);
+    document.getElementById('chatcontainer').appendChild(elem);
+});
+ }
 
 }
