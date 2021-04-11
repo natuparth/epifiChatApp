a62bb46fe9d77a42807f43e1191655e455109325
parth <parth.natu01@gmail.com>
2021-04-11 19:11:51 +0530

    final commit

--- js/main.js
+++ js/main.js
@@ -1,121 +1,153 @@
-// let personData = require('./personData').personData;
- //import { personData } from './personData.js'
- //var personData ;
 
- var funcCallCount = 0;
-function populateChatData(){
-  const processChange = debounce(() => saveInput());
+function populateChatData() {
+  const processChange = throttle((input) => searchMessages(input));
+
+  //adding listener for search bar input
   document.getElementById('search').addEventListener('input', processChange)
-  document.getElementById('chatcontainer').innerHTML="";
+  document.getElementById('chatcontainer').innerHTML = "";
+  document.getElementById('favourites').innerHTML = "";
   document.getElementById('search').hidden = true;
-       var readChatData = new ReadChatData();
-       document.getElementById('container').style.display = 'grid';
-      readChatData.readData().then((personData) => {
-        document.getElementById('individualChat').style.display = 'none';
-        var chatElement = document.getElementById('chatcontainer');
-        personData.forEach(element => {
-             var elem = document.createElement('div');
-             elem.classList.add('chat')
-             
-             var imageDiv = document.createElement('div')
-             imageDiv.style.justifySelf = 'center';
-             var imageTag = document.createElement('img')
-             imageTag.src = element.imageUrl
-             imageDiv.appendChild(imageTag)
-             var lastMsgDiv = document.createElement('div')
-             
-             var lastMsgObj = element.messages.their[element.messages.their.length - 1].timestamp 
-             >
-              element.messages.yours[element.messages.yours.length - 1].timestamp 
-              ?
-              element.messages.their[element.messages.their.length - 1]
-             : element.messages.yours[element.messages.yours.length - 1] ;
-             var lastMsg = lastMsgObj.message.length > 20 ? lastMsgObj.message.slice(0,20)+'...' :lastMsgObj.message ;
-             lastMsgDiv.innerHTML = "<div class='boldText largeText'> "+element.name+"</div>";
-             lastMsgDiv.innerHTML +="<div class='fadedText mediumText'>"+ lastMsg + "</div>";
-            // lastMsgDiv.classList.add('border-bottom');
-     
-             var lastMsgTime = new Date(lastMsgObj.timestamp).getHours() + ":" + new Date(lastMsgObj.timestamp).getMinutes();
-             var unreadMsg = document.createElement('div')
-             unreadMsg.innerHTML = "<p>"+lastMsgTime+"</p>";
-             unreadMsg.classList.add('timeAndUnread','boldText','mediumText')
-     
-             if(element.messages.unread == 0)
-              unreadMsg.classList.add('fadedText')
-             else            
-              unreadMsg.innerHTML += "<p class='unread'>"+ element.messages.unread.toString() + "</p>" ;
-             
-             var borderElem = document.createElement('div')
-             borderElem.className = 'border-bottom';
-             borderElem.style="grid-column:2/4; margin-right: 2em"
-             elem.appendChild(imageDiv)
-             elem.appendChild(lastMsgDiv);
-             elem.appendChild(unreadMsg);
-             elem.appendChild(borderElem)
-             elem.addEventListener('click', ()=>{openChatForUser(element.id)})
-             //  chatElement.onclick = openChatForUser(element.id);
-               chatElement.appendChild(elem);
-        });
-      })
-    
+  var readChatData = new ReadChatData();                        //defining an instance of class ReadChatData
+  populateFavourites(readChatData);                             //populating favourites container
+  document.getElementById('container').style.display = 'grid';
+  readChatData.readData().then((personData) => {                  //reading data from userData.json file using the method from ReadChatData class
+    document.getElementById('individualChat').style.display = 'none';
+    var chatElement = document.getElementById('chatcontainer');
+    personData.forEach(element => {
+      var chatListItem = createChatItem(element);         //creating chatListItems for all friends
+      chatElement.appendChild(chatListItem);
+    });
+  })
+
 }
 
 window.onload = populateChatData;
-function openChatForUser(id){
-    document.getElementById('container').style.display= 'none';
-    document.getElementById('individualChat').style.display = 'grid';
-     populateContentForIndividualChat(id);
-    
-}
-
-function populateContentForIndividualChat(id){
-    document.getElementById('individualChat').innerHTML="";
-    var readChatData = new ReadChatData();
-     var nameHeader = document.createElement('div')
-     var arrow = document.createElement('span');
-     arrow.addEventListener('click',()=>{
-       populateChatData();
-     })
-     arrow.style.float = 'left';
-     arrow.innerHTML = '<i class="fa fa-arrow-left" style="margin-top: 1em"></i>';
-     nameHeader.appendChild(arrow);
-     var nameDiv = document.createElement('div')
-     nameDiv.setAttribute('id', "chatName");
-     nameDiv.className = 'boldText';
-     nameDiv.style = 'float: left; margin-left: 1em; width: 90%; font-size: 25px';
-     var activeNow = document.createElement('div');
-     activeNow.classList.add('fadedText')
-     activeNow.innerHTML = "<span style='font-size: 15px'>Active Now</span><span><i class='fa fa-circle' style='font-size:10px;color:green; margin-left: 5px'></i>";
-     activeNow.style = "margin-left: 2.5em"
-    // nameDiv.innerText = personData.filter((elem) => elem.id === id)[0].name;
-     nameHeader.appendChild(nameDiv);
-     nameHeader.appendChild(activeNow);
-     document.getElementById('individualChat').appendChild(nameHeader);
-     readChatData.loadIndividualData(id);
-
-
+function openChatForUser(id) {                        //Used to toggle the displays between chat list and individual chats
+  document.getElementById('container').style.display = 'none';
+  document.getElementById('individualChat').style.display = 'grid';
+  populateContentForIndividualChat(id);
 
 }
 
-function searchBarVisiblity(status){
-  if(status == "visible")
-  document.getElementById('search').hidden = false;
-  else 
-  { 
-    document.getElementById('search').hidden = true;
+function createChatItem(element) {
+  var elem = document.createElement('div');                       //Creating a div for each person 
+  elem.classList.add('chat')
+
+  var imageDiv = document.createElement('div')
+  imageDiv.style.justifySelf = 'center';
+  var imageTag = document.createElement('img')
+  imageTag.src = element.imageUrl
+  imageDiv.appendChild(imageTag)
+  var lastMsgDiv = document.createElement('div')
+
+  var lastMsgObj = element.messages.their[element.messages.their.length - 1].timestamp   //Finding the last msg based on timestamp
+    >
+    element.messages.yours[element.messages.yours.length - 1].timestamp
+    ?
+    element.messages.their[element.messages.their.length - 1]
+    : element.messages.yours[element.messages.yours.length - 1];
+  var lastMsg = lastMsgObj.message.length > 20 ? lastMsgObj.message.slice(0, 20) + '...' : lastMsgObj.message;   //slicing the msg if length>20
+  lastMsgDiv.innerHTML = "<div class='boldText largeText'> " + element.name + "</div>";
+  lastMsgDiv.innerHTML += "<div class='fadedText mediumText'>" + lastMsg + "</div>";
+
+
+  var lastMsgTime = new Date(lastMsgObj.timestamp).getHours() + ":" + new Date(lastMsgObj.timestamp).getMinutes();
+  var unreadMsg = document.createElement('div')
+  unreadMsg.innerHTML = "<p style='margin-left: 1.0em'>" + lastMsgTime + "</p>";
+  unreadMsg.classList.add('timeAndUnread', 'boldText', 'mediumText')
+
+  if (element.messages.unread == 0)
+    unreadMsg.classList.add('fadedText')
+  else
+    unreadMsg.innerHTML += "<p class='unread'>" + element.messages.unread.toString() + "</p>";
+
+  var borderElem = document.createElement('div')
+  borderElem.className = 'border-bottom';
+  borderElem.style = "grid-column:2/4; margin-right: 2em"
+  elem.appendChild(imageDiv)
+  elem.appendChild(lastMsgDiv);
+  elem.appendChild(unreadMsg);
+  elem.appendChild(borderElem)
+  elem.addEventListener('click', () => { openChatForUser(element.id) })
+  return elem;
+}
+function populateContentForIndividualChat(id) {
+  document.getElementById('individualChat').innerHTML = "";
+  var readChatData = new ReadChatData();
+  var nameHeader = document.createElement('div')
+  var arrow = document.createElement('span');
+  arrow.addEventListener('click', () => {
+    populateChatData();
+  })
+  arrow.style = 'float: left; margin-top: 0.7em';
+  arrow.innerHTML = '<i style="margin-top: 1em"><img class="icon" src="./assets/icons/back.jpg"></i>';
+  var dotIcons = document.createElement('div')
+  dotIcons.style = "float: right";
+  dotIcons.innerHTML = "<img class='icon' src='./assets/icons/dots.jfif'>"
+  nameHeader.appendChild(arrow);
+  nameHeader.appendChild(dotIcons);
+  var nameDiv = document.createElement('div')
+  nameDiv.setAttribute('id', "chatName");
+  nameDiv.className = 'boldText';
+  nameDiv.style = 'float: left; margin-left: 0.4em; width: 80%; font-size: 25px';
+  var activeNow = document.createElement('div');
+  activeNow.classList.add('fadedText')
+  activeNow.innerHTML = "<span style='font-size: 15px'>Active Now</span><span><img src='../assets/icons/greenDot.png' class='smallIcon' style='font-size:10px;color:green; margin-left: 5px'>";
+  activeNow.style = "margin-left: 2.5em"
+  nameHeader.appendChild(nameDiv);
+  nameHeader.appendChild(activeNow);
+  document.getElementById('individualChat').appendChild(nameHeader);
+  readChatData.loadIndividualData(id);       //Loading messages for an individual chat
+}
+
+function searchBarVisiblity(status) {
+  if (status == "visible")
+    document.getElementById('search').style.display = 'flex';
+  else {
+    document.getElementById('search').style.display = 'none';
     populateChatData();
   }
-  }
+}
 
-  function debounce(func, timeout = 1500){
+function throttle(getFilteredMessages, timeout = 1000) {               //Function used to throttle requests withing 1 second(in case a user is typing very fast)
   let timer;
-  return (...args) => {
+  return (input) => {
     clearTimeout(timer);
-    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    timer = setTimeout(() => { getFilteredMessages.apply(this, input); }, timeout);
   };
 }
-function saveInput(){
+function searchMessages() {                                             //Search messages based on query
   var readChatData = new ReadChatData();
   readChatData.searchMessages(document.getElementById('searchInput').value);
+}
+function populateFavourites(readChatData) {            //Function for populating favourites container
+  var favouritesContainer = document.getElementById('favourites');
+  var favouritesText = document.createElement('span')
+  favouritesText.style = "font-size: small; font-family: cursive";
+  favouritesText.className = "fadedText";
+  favouritesText.innerText = "Favourites";
+  favouritesContainer.appendChild(favouritesText);
+  var favouritesList = document.createElement('div')
+  favouritesList.className = "favPeople";
+  favouritesList.style = "font-family: cursive"
+  readChatData.readFavourites().then((favourites) => {                    //Calling readFavourites method from class ReadChatData class
+    favourites.forEach((elem) => {
+      var favElement = document.createElement('div')
+      favElement.className = "person";
+
+      if (elem.active == true)
+        favElement.innerHTML = '<img class="selected" src="' + elem.imageUrl + '">' + '<span style="font-size: small">'+elem.name+'</span>';
+      else
+        favElement.innerHTML = '<img src="' + elem.imageUrl + '">' + '<span style="font-size: small" >' + elem.name +' </span>'
+      favouritesList.appendChild(favElement)
+
+
+    })
+    favouritesContainer.appendChild(favouritesList);
+    var bottomBorder = document.createElement('div')
+    bottomBorder.className = "border-bottom"
+    favouritesContainer.appendChild(bottomBorder)
+  })
+
 }
 
--- service/readChatData.js
+++ service/readChatData.js
@@ -1,111 +1,123 @@
-class ReadChatData{
-    constructor(){
-       
-    }
- readData(){
-   return fetch("../data/userData.json")
-    .then(response => {
-       return response.json();
-     })
- }
+class ReadChatData {
+  constructor() {
 
- loadIndividualData(id){
-     this.readData().then(data =>{
-       var personObj =  data.filter(element => element.id === id)[0];
-       var messages = personObj.messages.their.map(elem => {
-                  return {'owner': 'their',...elem}})
-            .concat(personObj.messages.yours.map(elem => {
-                  return {'owner': 'yours',...elem}})).sort((d1, d2) => new Date(d1.timestamp) - new Date(d2.timestamp));
-      
-        document.getElementById('chatName').innerHTML = personObj.name;
-         var chatDiv = document.createElement('div')
-         chatDiv.style = 'overflow: auto'
-         var chatHtml ='';
-         messages.forEach(element => {
-            var className; 
-            var time = new Date(element.timestamp).getHours() + ":" + new Date(element.timestamp).getMinutes();
-            if(element.owner === 'their')
-            chatHtml += '<div class="their"><p><img src="'+ personObj.imageUrl +'"></img></p><div>'+ element.message+'</div><span class="fadedText time">'+ time+'</span></div>'
+  }
+  readData() {                                //reading the entire userData.json file
+    return fetch("../data/userData.json")
+      .then(response => {
+        return response.json();
+      })
+  }
 
-            else
-            chatHtml += '<div class="yours"><div>'+ element.message+'</div><span class="fadedText">'+ time+'</span></div>'
-         
-         });
-        //  chatHtml += '<input type="text">'
-         chatDiv.innerHTML = chatHtml;
-         document.getElementById('individualChat').appendChild(chatDiv);
-         var inputDiv = document.createElement('div');
-         inputDiv.classList.add('textAreaInput');
-       //  inputDiv.innerHTML = '<input type="text" placeholder="Type your message" >'
-       var msgElement = document.createElement('textarea')
-       msgElement.setAttribute('id', 'messageArea');
-       msgElement.setAttribute('placeholder', 'Type new message');
-       
-       
-       msgElement.addEventListener('input',()=>{
-        
-          msgElement.style.height = ""; 
-          msgElement.style.height = Math.min(msgElement.scrollHeight, 200) + "px";
-           })
-           inputDiv.innerHTML += '<div class="sendIcon"> <i style="color: white;" class="fa fa-paper-plane"></i></div>'
-           inputDiv.appendChild(msgElement)
-         document.getElementById('individualChat').appendChild(inputDiv)
+  loadIndividualData(id) {
+    this.readData().then(data => {
+      var personObj = data.filter(element => element.id === id)[0];
+      var messages = personObj.messages.their.map(elem => {                //Fetching msgs from the json file and adding a field owner(thier, yours)
+        return { 'owner': 'their', ...elem }
+      })
+        .concat(personObj.messages.yours.map(elem => {
+          return { 'owner': 'yours', ...elem }
+        })).sort((d1, d2) => new Date(d1.timestamp) - new Date(d2.timestamp));
 
-     })
- }
+      document.getElementById('chatName').innerHTML = personObj.name;
+      var chatDiv = document.createElement('div')
+      chatDiv.style = 'overflow: auto'
+      var chatHtml = '';
+      messages.forEach(element => {
 
- searchMessages(input){
-   this.readData().then(data => {
-    var messages=[];
-     data.forEach(person => {
-       messages.push(...person.messages.their.concat(person.messages.yours).map((msg)=> {return  {'imageUrl': person.imageUrl,'name' : person.name, ...msg}}))
-     })
-    
-    this.loadSearchData(messages.filter(elem => elem.message.includes(input)), input);
-   })
-    
- }
- loadSearchData(data, query){
-  document.getElementById('chatcontainer').innerHTML = "";
-  data.forEach(element => {
-    var elem = document.createElement('div');
-    elem.classList.add('chat')
-    
-    var imageDiv = document.createElement('div')
-    imageDiv.style.justifySelf = 'center';
-    var imageTag = document.createElement('img')
-    imageTag.src = element.imageUrl
-    imageDiv.appendChild(imageTag)
-    var lastMsgDiv = document.createElement('div')
-    //var lastMsg = element.message.length > 20 ? element.message.slice(0,20)+'...' :element.message ;
-    var lastMsg = element.message;
-    var index = lastMsg.indexOf(query);
-    lastMsgDiv.innerHTML = "<div class='boldText largeText'> "+element.name+"</div>";
-    lastMsgDiv.innerHTML +="<div class='fadedText mediumText'>"+ lastMsg.substring(index-20>0?index-20:0,index) + 
-    "<span class='highlight'>" + lastMsg.substring(index,index+query.length) + "</span>" 
-    + lastMsg.substring(index+query.length,lastMsg.length>20? index+query.length: lastMsg.length) + "</div>";
-    //lastMsgDiv.classList.add('border-bottom');
+        var time = new Date(element.timestamp).getHours() + ":" + new Date(element.timestamp).getMinutes();
+        if (element.owner === 'their')
+          chatHtml += '<div class="their"><p><img src="' + personObj.imageUrl + '"></img></p><div>' + element.message + '</div><span class="fadedText time">' + time + '</span></div>'
 
-    var lastMsgTime = new Date(element.timestamp).getHours() + ":" + new Date(element.timestamp).getMinutes();
-    var unreadMsg = document.createElement('div')
-    unreadMsg.innerHTML = "<p>"+lastMsgTime+"</p>";
-    //unreadMsg.classList.add('boldText')
+        else
+          chatHtml += '<div class="yours"><div>' + element.message + '</div><span class="fadedText">' + time + '</span></div>'
 
-    
-     unreadMsg.classList.add('timeAndUnread','fadedText','mediumText')
-     var borderElem = document.createElement('div')
-     borderElem.className = 'border-bottom';
-     borderElem.style="grid-column:2/4; margin-right: 2em";
-    
-   
-    elem.appendChild(imageDiv)
-    elem.appendChild(lastMsgDiv);
-    elem.appendChild(unreadMsg);
-    elem.appendChild(borderElem)
-    //elem.addEventListener('click', ()=>{openChatForUser(element.id)})
-    //  chatElement.onclick = openChatForUser(element.id);
-    document.getElementById('chatcontainer').appendChild(elem);
-});
- }
+      });
 
+      chatDiv.innerHTML = chatHtml;
+      document.getElementById('individualChat').appendChild(chatDiv);
+      var inputDiv = document.createElement('div');
+      inputDiv.classList.add('textAreaInput');
+
+      var msgElement = document.createElement('textarea')
+      msgElement.setAttribute('id', 'messageArea');
+      msgElement.setAttribute('placeholder', 'Type new message');
+
+
+      msgElement.addEventListener('input', () => {
+
+        msgElement.style.height = "";
+        msgElement.style.height = Math.min(msgElement.scrollHeight, 200) + "px";   //fixing the max height of input to 200px
+      })
+
+      inputDiv.innerHTML += '<div class="sendIcon"> <img class="icon" style="blueviolet: white" src="../assets/icons/send.png"></div>'
+      inputDiv.appendChild(msgElement)
+      document.getElementById('individualChat').appendChild(inputDiv)
+
+    })
+  }
+
+  searchMessages(input) {
+    this.readData().then(data => {
+      var messages = [];
+      data.forEach(person => {
+        messages.push(...person.messages.their.concat(person.messages.yours).map(
+          (msg) => {
+            return { 'imageUrl': person.imageUrl, 'name': person.name, ...msg }
+          }))
+      }
+      )
+
+      this.loadSearchData(messages.filter(elem => elem.message.includes(input)), input);
+    })
+
+  }
+  loadSearchData(data, query) {
+    document.getElementById('chatcontainer').innerHTML = "";
+    data.forEach(element => {
+      var elem = document.createElement('div');
+      elem.classList.add('chat')
+
+      var imageDiv = document.createElement('div')
+      imageDiv.style.justifySelf = 'center';
+      var imageTag = document.createElement('img')
+      imageTag.src = element.imageUrl
+      imageDiv.appendChild(imageTag)
+      var lastMsgDiv = document.createElement('div')
+      //var lastMsg = element.message.length > 20 ? element.message.slice(0,20)+'...' :element.message ;
+      var lastMsg = element.message;
+      var index = lastMsg.indexOf(query);
+      lastMsgDiv.innerHTML = "<div class='boldText largeText'> " + element.name + "</div>";
+      lastMsgDiv.innerHTML += "<div class='fadedText mediumText'>" + lastMsg.substring(index - 20 > 0 ? index - 20 : 0, index) +
+        "<span class='highlight'>" + lastMsg.substring(index, index + query.length) + "</span>"
+        + lastMsg.substring(index + query.length, lastMsg.length > 20 ? index + query.length : lastMsg.length) + "</div>";
+      //lastMsgDiv.classList.add('border-bottom');
+
+      var lastMsgTime = new Date(element.timestamp).getHours() + ":" + new Date(element.timestamp).getMinutes();
+      var unreadMsg = document.createElement('div')
+      unreadMsg.innerHTML = "<p>" + lastMsgTime + "</p>";
+      //unreadMsg.classList.add('boldText')
+
+
+      unreadMsg.classList.add('timeAndUnread', 'fadedText', 'mediumText')
+      var borderElem = document.createElement('div')
+      borderElem.className = 'border-bottom';
+      borderElem.style = "grid-column:2/4; margin-right: 2em";
+
+
+      elem.appendChild(imageDiv)
+      elem.appendChild(lastMsgDiv);
+      elem.appendChild(unreadMsg);
+      elem.appendChild(borderElem)
+      //elem.addEventListener('click', ()=>{openChatForUser(element.id)})
+      //  chatElement.onclick = openChatForUser(element.id);
+      document.getElementById('chatcontainer').appendChild(elem);
+    });
+  }
+  readFavourites() {
+    return fetch("../data/favourites.json")
+      .then(response => {
+        return response.json();
+      })
+  }
 }
